fe93cdc942c9667c9292224dacbac23ae714e761
RadioAddition <radioaddition@proton.me>
2024-11-02 19:52:52 -0400

    Update install_cliphist.sh

--- files/scripts/install_cliphist.sh
+++ files/scripts/install_cliphist.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -oue pipefail
 wget https://github.com/sentriz/cliphist/releases/download/v0.6.1/v0.6.1-linux-amd64 -O cliphist
 chmod a+x ./cliphist
 mv ./cliphist /usr/bin/
